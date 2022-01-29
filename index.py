c7c61cc683c12800d4e9f454fdfc475e40208436
DjibrilNaji <najidjibril@gmail.com>
2022-01-29 11:47:48 +0100

    testtesttest

--- index.py
+++ index.py
@@ -11,24 +11,25 @@ CORS(app)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
-  
-@app.route('/chambre/<id>', methods=['GET']) 
-def ma_fonction(id): 
-  print(id)
-  con = sql.connect("hotelcaraibes")
-  con.row_factory = sql.Row
-  cur = con.cursor()
-  cur.execute("select * from chambre where id=:id", {"id":id})
-  data=[]
-  rows = cur.fetchall(); 
-  for row in rows:
-    data.append(list(row))
-  if len(data) != 0:
-    return jsonify(data)
-  else:
-    return json.dumps({'error':str(data[0])})
-  conn.close()
+    return "Hello World!"
 
-# if __name__ == '__main__':
-#  app.run(debug=True)
+
+@app.route('/chambre/<id>', methods=['GET'])
+def ma_fonction(id):
+    print(id)
+    con = sql.connect("hotelcaraibes")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from chambre where id=:id", {"id": id})
+    data = []
+    rows = cur.fetchall();
+    for row in rows:
+        data.append(list(row))
+    if len(data) != 0:
+        return jsonify(data)
+    else:
+        return json.dumps({'error': str(data[0])})
+    conn.close()
+
+if __name__ == '__main__':
+    app.run(debug=True)
